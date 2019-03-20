6ab1b04a470b1dd96400653a8ca3cdc249712e08
oppressionslayer <37212196+oppressionslayer@users.noreply.github.com>
2019-03-20 08:45:07 -0600

    Update gods.py

--- gods.py
+++ gods.py
@@ -1,5 +1,10 @@
 # Copyright William Lars Rocha
 #
+# Infinite Compression. 
+# 
+# Commercial Use Available with my permission in writing and royalties
+# will be negotiated.
+#
 # The license for this is permissive. You must pay me royalties to use these concepts.
 # No commercial use of this or its concepts are allowed without my permission.
 # Free for personal or Academic use. All forks must remain public. thanks.
@@ -106,3 +111,61 @@ def gods_interconnected_structure(godsshift, godshiddenshift, position):
          #print(godsnum, (godshiddeninformation, godsnum, godshiddennumdetermined), ( godshiddeninformation, godsnum ) )
      return (godsnum, godshiddennum, godshiddennumdetermined, godsorder, godsdisorder, godsnum==int(sshex[position],16), godshiddeninformation)
        
+     
+def godping(val,bitsize=4, godmod=14): 
+     val = twos_complement(val,bitsize) 
+     uint_val_maskmod=val&godmod 
+     uint_val_modulus=val%godmod 
+     uint_val = ( uint_val_maskmod, uint_val_modulus ) 
+     binary = bin(val) 
+     val_bin = ( val, uint_val_maskmod, binary, bin(~val + 1) ) 
+     non_neg_complement = ( 15-(~(~val + 1))&godmod, 15-(~(~val + 1))%godmod ) 
+     neg_complement = ( 15-(~(~-val + 1))&godmod, 15-(~(~-val + 1))%godmod ) 
+     non_neg_val = ( (~(~val + 1))&godmod, (~(~val + 1))%godmod )  
+     neg_val = ( (~(~-val + 1))&godmod, (~(~-val + 1))%godmod )  
+     no_mod_non_neg_val = ( (~(~val + 1)), (~(~val + 1)) ) 
+     no_mod_neg_val = ( (~(~-val + 1)), (~(~-val + 1)) ) 
+     return [ val_bin, uint_val,  non_neg_complement,  
+             neg_complement, non_neg_val, neg_val, 
+             no_mod_non_neg_val, no_mod_neg_val, 
+             ] 
+     
+
+# open a file even random data, which can be infinitely recompressed with zip after saving.
+# godarray: bitstring.BitArray(hex=godsarray).tofile(distfile) then zip godarray.zip godsarray.
+# you can infinitely do this. uncompress by using godcompress(godsarray) and then compare 
+# gods == sshex
+# 
+# Infinite compression. Copyright and Permission to use commercially must have my permission.
+    
+utility, build, sscmp, rrcmp, godlike, godsdistance, godsrandom, gods = [], [], '', '', '', [], '',''
+godsarray = ''
+for x in range(0,len(sshex)): 
+     reciprical  = godping(int(sshex[x],16), 4, 0xF)[0][1:3] 
+     utility.append( (ast.literal_eval(reciprical[1])) ) #   
+     godbuild = godping(reciprical[0], 4, 0xF)[0][0:2]   
+     build.append(godbuild)
+     godshiddennum = abs(reciprical[0]-abs(build[x][1]))
+     godscards = (reciprical[0], abs(build[x][1]))
+     godsdistance.append(godshiddennum)
+     if godbuild[0] < godbuild[1]:
+        godsdice = abs(twos_complement(godsdistance[x], bits=3))
+        if godsdice != godsdistance[x]:
+           godsdice = godsdistance[x] >> 1
+        godsreciprical = Bits(uint=godsdice, length=4).bin[0:4]
+     else:
+        godsdice = abs(twos_complement(godsdistance[x], bits=2))
+        godsreciprical = Bits(uint=godsdice, length=4).bin[0:4]
+     if godsdice < int(godsreciprical,16):
+        godsnum = godscards[1]
+     else:
+        godsnum = godscards[0]
+     godsrandom += godsreciprical
+     godsarray += hex(int(godsreciprical,2))[-1]
+     gods += hex(godsnum)[-1]
+     if x < 128:
+        print(godbuild, reciprical[0], reciprical[1], reciprical,utility[x],
+              build[x],abs(build[x][1]), godsdistance[x], godsreciprical, godsdice, godsnum, abs(build[x][1]) == godsnum, godscards) 
+     sscmp += hex(abs(build[x][1]))[-1]
+     rrcmp += hex(reciprical[0])[-1]
+
